b5180f14848f67a243e30f32319cf82097c602da
DevBrianKE <kipchumbabrian47@gmail.com>
2026-01-30 16:03:43 +0300

    Connect ApprovalStats.jsx to adminSlice to show pending approvals

--- client/src/pages/admin/ApprovalStats.jsx
+++ client/src/pages/admin/ApprovalStats.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPendingApprovals } from '../../features/adminSlice';
 
-// Admin page to view and manage pending approvals
+// Admin page to view and manage pending product/service approvals
 const ApprovalStats = () => {
+  const dispatch = useDispatch();
+  const { pendingApprovals } = useSelector((state) => state.admin);
+
+  // Load demo pending approvals (replace with API call later)
+  useEffect(() => {
+    const demoApprovals = [
+      { id: 1, type: 'product', name: 'Cat Food' },
+      { id: 2, type: 'service', name: 'Dog Vaccination' },
+    ];
+    dispatch(setPendingApprovals(demoApprovals));
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Approval Stats</h1>
-      <p>Pending product and service approvals will be displayed here.</p>
+      <ul>
+        {pendingApprovals.map((item) => (
+          <li key={item.id}>
+            {item.type.toUpperCase()}: {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
