04f206ce4fdb329700d61d08ec9ced88a6095a8f
DevBrianKE <kipchumbabrian47@gmail.com>
2026-01-30 15:13:36 +0300

    Add admin ApprovalStats.jsx to track pending approvals

---
+++ client/src/pages/admin/ApprovalStats.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Admin page to view and manage pending approvals
+const ApprovalStats = () => {
+  return (
+    <div>
+      <h1>Approval Stats</h1>
+      <p>Pending product and service approvals will be displayed here.</p>
+    </div>
+  );
+};
+
+export default ApprovalStats;
