e6816edc80d9c0461f41f9882e94581bd8fc3c92
Collins Kibet <kollcibe05@gmail.com>
2026-02-05 10:16:15 +0300

    minor updates

--- client/src/store.js
+++ client/src/store.js
@@ -3,12 +3,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
 import cartReducer from './features/cartSlice';
 import uiReducer from './features/uiSlice';
+import serviceReducer from "./features/serviceSlice"
+import productReducer from "./features/productSlice"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
     ui: uiReducer,
+    services: serviceReducer,
+    products: productReducer,
   },
   
   devTools: process.env.NODE_ENV !== 'production',
