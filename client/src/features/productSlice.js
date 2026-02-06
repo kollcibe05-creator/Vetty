015e6e50580f4f583464833ba1878730ad44ffac
Collins Kibet <kollcibe05@gmail.com>
2026-02-06 10:33:32 +0300

    spinner, notifications, CRUD functionality

--- client/src/features/productSlice.js
+++ client/src/features/productSlice.js
@@ -1,24 +1,149 @@
 import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import { showSpinner, showNotification, hideSpinner } from "./uiSlice"
 
-export const fetchProducts = createAsyncThunk("products/fetchAll", (categoryId = null) => {
+export const fetchProducts = createAsyncThunk("products/fetchAll", (categoryId = null, {dispatch, rejectWithValue}) => {
     const url = categoryId ? `/products?category_id=${categoryId}` : '/products'
+    dispatch(showSpinner({message: "Loading products..."}))
     return fetch(url)
     .then((r) => {
         if(!r.ok) throw new Error("Failed to fetch products")
         return r.json()
     })
-    .then (data => data)    
+    .then (data => {
+       dispatch(hideSpinner()) 
+       return data
+
+    })   
+    .catch(err => {
+        dispatch(hideSpinner())
+        dispatch(showNotification({
+            type: "error",
+            title: "Fetch Error",
+            message: err.message
+        }))
+        return rejectWithValue(err.message)
+    }) 
 })
 
 
-export const fetchProductById = createAsyncThunk("products/fetchById", (id) => {
+export const fetchProductById = createAsyncThunk("products/fetchById", (id, {dispatch, rejectWithValue}) => {
+    dispatch(showSpinner({message: "Fetching product details..."}))
     return fetch(`/products/${id}`)
     .then(r => {
         if (!r.ok) throw  new Error("Product not found")
         return r.json()    
         })
-    .then(data => data)    
+    .then(data => {
+
+        dispatch(hideSpinner())
+        return data
+
+    })    
+    .catch(err => {
+        dispatch(hideSpinner())
+        dispatch(showNotification({
+           type: 'error',
+           title: 'Not Found',
+           message: err.message 
+           
+        }))
+        return rejectWithValue(err.message)
+    })
 })
+export const patchProduct = createAsyncThunk(
+    "products/patch", (formData, {dispatch, rejectWithValue}) => {
+        const {id, ...fields} = formData;
+        dispatch(showSpinner({message: "Saving Changes..."}))
+        return fetch(`/products/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(fields),
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Failed to update product")
+            return r.json()
+        })
+        .then(data => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'success',
+                message: "Product updated!"
+            }))
+            return data
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'error',
+                title: 'Update Error',
+                message: err.message  
+            }))
+            return rejectWithValue(err.message)
+        })
+    }
+    
+)
+export const postProduct = createAsyncThunk(
+    "products/post", (formData, {dispatch, rejectWithValue}) => {
+        const {id, ...fields} = formData;
+        dispatch(showSpinner({message: "Saving Changes..."}))
+        return fetch("/products", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(fields),
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Failed to post product")
+            return r.json()
+        })
+        .then(data => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'success',
+                message: "Product posted!"
+            }))
+            return data
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'error',
+                title: 'Post Error',
+                message: err.message  
+            }))
+            return rejectWithValue(err.message)
+        })
+    }
+    
+)
+
+export const deleteProduct = createAsyncThunk(
+    'products/delete', 
+    (id, {dispatch, rejectWithValue}) => {
+        dispatch(showSpinner({message: 'Deleting Product...'}))
+        return fetch(`/products/${id}`, {
+           method: "DELETE" 
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Could not delete product")
+            return r.status === 204? {id} : r.json();
+        })
+        .then(()=> {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: "success", 
+                message: "Product deleted Successfully!"
+            }))
+            return id 
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({type: 'error', title: 'Delete Failed', message: err.message}))
+            return rejectWithValue(err.message)
+        })
+        
+    }
+)
 
 const productSlice = createSlice({
     name: 'products',
@@ -51,7 +176,42 @@ const productSlice = createSlice({
         .addCase(fetchProductById.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
-      });
+      })
+      .addCase(patchProduct.pending, (state) => {
+               state.loading = true;
+               state.error = null;
+            })
+            .addCase(patchProduct.rejected, (state, action) => {
+              state.loading = false;
+              state.error = action.payload;
+            })
+      .addCase(patchProduct.fulfilled, (state, action) => {
+        const index = state.items.findIndex(p => p.id == action.payload.id)
+        if (index !== -1){
+            state.items[index] = action.payload
+        }
+      }).addCase(deleteProduct.pending, (state) => {
+               state.loading = true;
+               state.error = null
+            })
+            .addCase(deleteProduct.rejected, (state, action) => {
+              state.loading = false;
+              state.error = action.payload;
+            })
+      .addCase(deleteProduct.fulfilled, (state, action) => {
+        state.items = state.items.filter(product => product.id !== action.payload)
+      })
+      .addCase(postProduct.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(postProduct.rejected, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(postProduct.fulfilled, (state, action) => {
+        state.items = [...state.items, action.payload]
+      })
     }
 })
 
--- client/src/features/serviceSlice.js
+++ client/src/features/serviceSlice.js
@@ -1,28 +1,152 @@
-import {createSlice, createAsyncThunk, isRejectedWithValue} from "@reduxjs/toolkit"
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit"
+import { hideSpinner, showNotification, showSpinner } from "./uiSlice"
 
 
-export const fetchServices = createAsyncThunk("services/fetchAll", (categoryId = null) => {
+export const fetchServices = createAsyncThunk("services/fetchAll", (categoryId = null, {dispatch, rejectWithValue}) => {
     const url = categoryId ? `/services?category_id=${categoryId}` : '/services'
+    dispatch(showSpinner({message: "Loading services..."}))
     return fetch(url)
     .then((r) => {
         if(!r.ok) throw new Error("Failed to fetch services")
         return r.json()
     })
-    .then (data => data) 
+    .then (data => {
+        
+        dispatch(hideSpinner())
+        return data
+    }) 
     .catch (err => {
-       return (err.message)
-    }  )
+        dispatch(hideSpinner())
+        dispatch(showNotification({
+            type: 'error',
+            title: 'Fetch Error',
+            message: 'Failed to fetch services'
+        }))
+        return rejectWithValue(err.message)
+    })
 })
 
 
-export const fetchServiceById = createAsyncThunk("services/fetchById", (id) => {
+export const fetchServiceById = createAsyncThunk("services/fetchById", (id, {dispatch, rejectWithValue}) => {
+    dispatch(showSpinner({message: "Fetching service detail..."}))
     return fetch(`/services/${id}`)
     .then(r => {
         if (!r.ok) throw  new Error("Product not found")
         return r.json()    
         })
-    .then(data => data)    
+    .then(data => {
+        dispatch(hideSpinner())
+        return data
+    }) 
+    .catch(err => {
+        dispatch(hideSpinner())
+        dispatch(showNotification({
+            type: 'error',
+            title: 'Fetch Error',
+            message: err.message || 'Failed to fetch service detail'
+        }))
+        return rejectWithValue(err.message)
+    })   
 })
+
+export const patchService = createAsyncThunk(
+    "services/patch", (formData, {dispatch, rejectWithValue}) => {
+        const {id, ...fields} = formData;
+        dispatch(showSpinner({message: "Saving Changes..."}))
+        return fetch(`/services/${id}`, {
+            method: "PATCH",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(fields),
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Failed to update service")
+            return r.json()
+        })
+        .then(data => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'success',
+                message: "Service updated!"
+            }))
+            return data
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'error',
+                title: 'Update Error',
+                message: err.message  
+            }))
+            return rejectWithValue(err.message)
+        })
+    }
+    
+)
+export const deleteService = createAsyncThunk(
+    'services/delete', 
+    (id, {dispatch, rejectWithValue}) => {
+        dispatch(showSpinner({message: 'Deleting Service...'}))
+        return fetch(`/services/${id}`, {
+           method: "DELETE" 
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Could not delete Service")
+            return r.status === 204? {id} : r.json();
+        })
+        .then(()=> {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: "success", 
+                message: "Service deleted Successfully!"
+            }))
+            return id 
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({type: 'error', title: 'Delete Failed', message: err.message}))
+            return rejectWithValue(err.message)
+        })
+        
+    }
+)
+
+export const postService = createAsyncThunk(
+    "services/post", (formData, {dispatch, rejectWithValue}) => {
+        const {id, ...fields} = formData;
+        dispatch(showSpinner({message: "Saving Changes..."}))
+        return fetch("/services", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(fields),
+        })
+        .then(r => {
+            if (!r.ok) throw new Error("Failed to post service")
+            return r.json()
+        })
+        .then(data => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'success',
+                message: "Service posted!"
+            }))
+            return data
+        })
+        .catch(err => {
+            dispatch(hideSpinner())
+            dispatch(showNotification({
+                type: 'error',
+                title: 'Post Error',
+                message: err.message  
+            }))
+            return rejectWithValue(err.message)
+        })
+         
+    }
+    
+)
+
+
+
 
 const serviceSlice = createSlice({
     name: 'services',
@@ -55,7 +179,43 @@ const serviceSlice = createSlice({
         .addCase(fetchServiceById.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
-      });
+      })
+      .addCase(patchService.pending, (state) => {
+         state.loading = true;
+         state.error = null;
+      })
+      .addCase(patchService.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+       .addCase(patchService.fulfilled, (state, action) => {
+              const index = state.list.findIndex(p => p.id == action.payload.id)
+              if (index !== -1){
+                  state.list[index] = action.payload
+              }
+            })
+        .addCase(deleteService.pending, (state) => {
+            state.loading = true;
+            state.error = null
+      })
+        .addCase(deleteService.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+        })
+        .addCase(deleteService.fulfilled, (state, action) => {
+        state.list = state.list.filter(product => product.id !== action.payload)
+        })
+    .addCase(postService.pending, (state) => {
+            state.loading = true
+            state.error = null
+    })
+    .addCase(postService.rejected, (state) => {
+    state.loading = true
+    state.error = null
+    })
+    .addCase(postService.fulfilled, (state, action) => {
+    state.list = [...state.list, action.payload]
+    })
     }
 })
 
