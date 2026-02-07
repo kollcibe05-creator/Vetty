236a75bcf3c295f3d0bc837336210f4a4d155dc6
Collins Kibet <kollcibe05@gmail.com>
2026-02-07 18:32:14 +0300

    final migration

---
+++ server/migrations/versions/9bbdf99e0333_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 9bbdf99e0333
+Revises: 45009e43b40b, 719b3a2b9c64
+Create Date: 2026-02-07 12:55:18.474713
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9bbdf99e0333'
+down_revision = ('45009e43b40b', '719b3a2b9c64')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
