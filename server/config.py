32b48bf003ee909c0a12e98b973ae4af208c3ef1
Collins Kibet <kollcibe05@gmail.com>
2026-02-12 23:38:38 +0300

    render template

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_restful import Api
