7fed561e98df6113a40297fe45d4fb0b6e41c0be
Collins Kibet <kollcibe05@gmail.com>
2026-02-13 10:00:47 +0300

    paw

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -27,9 +27,10 @@ const Navbar = () => {
           className="flex items-center gap-3 cursor-pointer group"
         >
           <div className="w-10 h-10 bg-yellow-400 rounded-xl flex items-center justify-center text-[#2D1B69] transform group-hover:rotate-12 transition-transform shadow-sm">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            🐾
+            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
               <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-            </svg>
+            </svg> */}
           </div>
           <span className="text-2xl font-black text-[#2D1B69] tracking-tighter uppercase">Vetty</span>
         </div>
